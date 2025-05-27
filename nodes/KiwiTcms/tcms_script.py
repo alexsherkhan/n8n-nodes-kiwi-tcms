c861f9f9824dd15fbe38af411ecf7049558a4804
g.bagaevski <g.bagaevski@fast-report.com>
2025-05-27 13:35:09 +0300

    fix_add_case_method

--- nodes/KiwiTcms/tcms_script.py
+++ nodes/KiwiTcms/tcms_script.py
@@ -4,12 +4,12 @@ from tcms_api import TCMS
 
 def main():
     try:
-        
+        import sys
+        import json
+        from tcms_api import TCMS
+
         raw_input = sys.stdin.read()
-        
-        
         args = json.loads(raw_input)
-        
         
         client = TCMS(
             url=args["url"],
@@ -17,18 +17,20 @@ def main():
             password=args["password"]
         )
         
-        
         obj, method = args["action"].split('.', 1)
         rpc_method = getattr(getattr(client.exec, obj), method)
         
         
-        if isinstance(args.get("params"), list):
-            result = rpc_method(*args["params"])
+        if args["action"] == "TestPlan.add_case":
+            result = rpc_method(args["params"]["plan_id"], args["params"]["case_id"])
         else:
-            result = rpc_method(args.get("params", {}))
+            
+            if isinstance(args.get("params"), list):
+                result = rpc_method(*args["params"])
+            else:
+                result = rpc_method(args.get("params", {}))
         
-        
-        print(json.dumps(result, default=str, ensure_ascii=False))
+        print(json.dumps(result, default=str))
         
     except Exception as e:
         error_info = {
