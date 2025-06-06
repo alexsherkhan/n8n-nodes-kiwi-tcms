7033082a824b3ee4df7fe828531c772d5c411ca8
g.bagaevski <g.bagaevski@fast-report.com>
2025-06-06 14:39:49 +0300

    add_TestRun_get_cases_support

--- nodes/KiwiTcms/tcms_script.py
+++ nodes/KiwiTcms/tcms_script.py
@@ -16,14 +16,25 @@ def main():
         obj, method = args["action"].split('.', 1)
         rpc_method = getattr(getattr(client.exec, obj), method)
         
-        
-        if args["action"] in ["TestPlan.add_case", "TestRun.add_case"]:
+
+        if args["action"] in [
+            "TestPlan.add_case",
+            "TestRun.add_case",
+            "TestRun.get_cases",  
+            "TestRun.status_counts"  
+        ]:
             if isinstance(args["params"], dict):
                 
                 if args["action"] == "TestPlan.add_case":
                     result = rpc_method(args["params"]["plan_id"], args["params"]["case_id"])
                 elif args["action"] == "TestRun.add_case":
                     result = rpc_method(args["params"]["run_id"], args["params"]["case_id"])
+                elif args["action"] == "TestRun.get_cases":
+                    
+                    run_id = args["params"].get("run_id") or args["params"].get("id")
+                    if not run_id:
+                        raise ValueError("Missing run_id or id in params")
+                    result = rpc_method(run_id)
             else:
                 
                 result = rpc_method(*args["params"])
